b0042cd97fc1e788e4f601fe2b41fca5dab6a096
Ryoichi Gokan <nargok@gmail.com>
2018-06-24 10:17:56 +0900

    modify variable name & args name in reducers/count.js

--- src/reducers/count.js
+++ src/reducers/count.js
@@ -1,9 +1,9 @@
 import { INCREMENT, DECREMENT } from '../src/actions'
 
-const intialState = { value: 0 }
+const initialState = { value: 0 }
 
 export default (state = initialState, action) => {
-  switch (action, type) {
+  switch (action.type) {
     case INCREMENT:
       return { value: state.value + 1 }
     case DECREMENT:
