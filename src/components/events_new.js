06b82919723a8a579a3386cc07c55a12411517cd
Ryoichi Gokan <nargok@gmail.com>
2018-07-01 15:48:27 +0900

    implement event new component

---
+++ src/components/events_new.js
@@ -0,0 +1,60 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux'
+import { Field, reduxForm } from 'redux-form'
+import { Link } from 'react-router-dom'
+
+import { postEvent } from '../actions'
+
+class EventsNew extends Component {
+  constructor(props) {
+    super(props)
+    this.onSubmit = this.onSubmit.bind(this)
+  }
+
+  renderField(field) {
+    const { input, label, type, meta: { touched, error } } = field
+    return (
+      <div>
+        <input {...input} placeholder={label} type={type}/>
+        {touched && error && <span>{error}</span>}
+      </div>)
+  }
+
+  // 非同期処理
+  async onSubmit(values) {
+    await this.props.postEvent(values)
+    // イベントの作成が終わったら、一覧画面へ遷移する
+    this.props.history.push('/') // TODO 後で調べる
+  }
+
+  render() {
+    // submitボタンの不活性化
+    // pristine   -> title, bodyが入力前のsubmitを防ぐ 
+    // submitting -> submitボタンの連打を防ぐ
+    const { handleSubmit, pristine, submitting } = this.props
+    return(
+      <form onSubmit={handleSubmit(this.onSubmit)}>
+        <div><Field label="Title" name="title" type="text" component={this.renderField} /></div>
+        <div><Field label="Body" name="body" type="text" component={this.renderField} /></div>
+
+        <div>
+          <input type="submit" value="Submit" disabled={pristine || submitting} />
+          <Link to="/">Cancel</Link>
+        </div>
+      </form>
+    )
+  }
+}
+
+const validate = values => {
+  const errors = {}
+  if (!values.title) errors.title = "Enter a title, please."
+  if (!values.body) errors.body = "Enter a body, please."
+  return errors
+}
+
+const mapDispatchToProps = ({ postEvent })
+
+export default connect(null, mapDispatchToProps)(
+  reduxForm({ validate, form: 'eventNewForm' })(EventsNew)
+)
